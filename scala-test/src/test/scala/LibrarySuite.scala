8cb7776385efe821af8fb70747263646b41dce05
amit <amitsk1974@gmail.com>
2014-09-05 21:26:53 -0700

    modifications for new libs

--- scala-test/src/test/scala/LibrarySuite.scala
+++ scala-test/src/test/scala/LibrarySuite.scala
@@ -6,13 +6,11 @@
  */
 
 import org.scalatest.FunSuite
-import org.junit.runner.RunWith
-import org.scalatest.junit.JUnitRunner
 
-@RunWith(classOf[JUnitRunner])
+//@RunWith(classOf[JUnitRunner])
 class LibrarySuite extends FunSuite {
   test("someLibraryMethod is always true") {
     def library = new Library()
-    assert(library.someLibraryMethod)
+    assert(library.someLibraryMethod )
   }
 }
