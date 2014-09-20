0e2a7cd934448132f408bc9fe6101c0f779f0948
amit <amitsk1974@gmail.com>
2014-09-20 16:59:14 -0700

    login now works!

--- scala-test/src/test/scala/com/github/webuitesting/LoginPageSpec.scala
+++ scala-test/src/test/scala/com/github/webuitesting/LoginPageSpec.scala
@@ -8,12 +8,11 @@ import org.scalatest.time.{Span, Seconds}
 /**
  * Created by amit on 9/20/14.
  */
-class LoginPageSpec extends WebBrowserBaseFeatureSpec {
+class LoginPageSpec extends WebBrowserBaseSpec {
   implicit val webDriver: WebDriver = new FirefoxDriver
 
 
-  feature("Login") {
-    scenario("User logins to the application") {
+    "A User " should "be able to login " in  {
 
       Given("The user goes to the login page")
       val loginPage = new LoginPage
@@ -22,7 +21,7 @@ class LoginPageSpec extends WebBrowserBaseFeatureSpec {
       When("Enter credentials and click login")
       val userNameField = textField(cssSelector("input#id_username"))
       val passwordField = pwdField(cssSelector("input#id_password"))
-      val submitButton = find("div.submit-row>input").get
+      val submitButton = find(cssSelector("div.submit-row>input")).get
       userNameField.value = "amit"
       passwordField.value = "password"
       click on submitButton
@@ -30,6 +29,9 @@ class LoginPageSpec extends WebBrowserBaseFeatureSpec {
       Then("User is logged in to the website")
         pageTitle contains("poll")
     }
-  }
 
+  // Delete the temp file
+  override def afterAll() {
+    close()
+  }
 }
--- scala-test/src/test/scala/com/github/webuitesting/WebBrowserBaseSpec.scala
+++ scala-test/src/test/scala/com/github/webuitesting/WebBrowserBaseSpec.scala
@@ -1,10 +1,11 @@
 package com.github.webuitesting
 
-import org.scalatest.{Matchers, FlatSpec}
+import org.scalatest._
 
 import org.scalatest.selenium.WebBrowser
 
 /**
  * Created by amit on 9/20/14.
  */
-abstract class WebBrowserBaseSpec extends FlatSpec with Matchers with WebBrowser
+abstract class WebBrowserBaseSpec extends FlatSpec with Matchers with GivenWhenThen with WebBrowser
+with BeforeAndAfterAll with BeforeAndAfter
