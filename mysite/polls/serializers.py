f3b26bcf1c7d3ced77857fc4292369e057cfba70
amitsk <amitsk1974@gmail.com>
2016-06-18 21:54:41 -0700

    CLean up unused folders

--- mysite/polls/serializers.py
+++
@@ -1,7 +0,0 @@
-from polls.models import Poll
-from rest_framework import serializers
-
-class PollsSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model=Poll
-        fields = ('id','question', 'pub_date')
